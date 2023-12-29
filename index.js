78ce1dbe999b681583dca3bea9ccea3bddf38571
AgnDzi <152863493+AgnDzi@users.noreply.github.com>
2023-12-29 11:26:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+let userRole = null;
+//potrzebuje cos, zeby robic zasiegi a tego nie trzeba tu czyscic a jak dam local storage to trzeba czyscic
+
+const loginFailedMsg = document.getElementById("pLogin");
+
 const ingredContent = document.getElementById("ingredContent");
 ingredContent.style.display = "none";
 
 const recipesContent = document.getElementById("recipesContent");
 recipesContent.style.display = "none";
 
+const loginForm = document.getElementById("loginForm");
+loginForm.style.display = "flex";
+
 const rightContent = document.getElementById("rightContent");
-rightContent.style.display = "flex";
+rightContent.style.display = "none";
+
+const leftMenu = document.getElementById("leftMenu");
+leftMenu.style.display = "none";
+
+const productList = document.getElementById("productList");
+productList.style.display = "none";
 
 const liIngred = document.getElementById("li-ingred");
 liIngred.onclick = function () {
   ingredContent.style.display = "flex";
   rightContent.style.display = "none";
   recipesContent.style.display = "none";
+  leftMenu.style.display = "block";
+  loginForm.style.display = "none";
 };
 
 const liRecipes = document.getElementById("li-recipes");
@@ -19,6 +35,8 @@ liRecipes.onclick = function () {
   recipesContent.style.display = "flex";
   rightContent.style.display = "none";
   ingredContent.style.display = "none";
+  leftMenu.style.display = "block";
+  loginForm.style.display = "none";
 };
 
 const ingredientsArray = [
@@ -184,3 +202,84 @@ recipesArray.forEach((recipe) => {
   recipeButtonContainer.className = "recpie-button-container";
   recipesContent.appendChild(recipeContainer);
 });
+
+const mockedLoginService = () => {
+  const usersArray = [
+    { login: "user1", password: "pass1", rola: "customer" },
+    { login: "user2", password: "pass2", rola: "cook" },
+  ];
+  const authenticateUser = (login, password) => {
+    const foundUser = usersArray.find(
+      (user) => user.login === login && user.password === password
+    );
+    if (foundUser) {
+      return { role: foundUser.rola };
+    } else {
+      return { errorMsg: "Incorrect password or login" };
+    }
+  };
+
+  return { authenticateUser };
+};
+
+//przyjmie parametr
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const loginName = event.target.elements.loginName.value;
+  const password = event.target.elements.password.value;
+
+  //target czyli tam gdzie jest obslugiwane a curenttarget to skad wyszlo- zrodlo
+  const serviceResponse = mockedLoginService().authenticateUser(
+    loginName,
+    password
+  );
+
+  userRole = serviceResponse.role ?? null;
+
+  if (serviceResponse.role === "cook") {
+    leftMenu.style.display = "block";
+    rightContent.style.display = "block";
+    loginForm.style.display = "none";
+    location.hash = "cook-view";
+  } else if (serviceResponse.role === "customer") {
+    productList.style.display = "flex";
+    loginForm.style.display = "none";
+    location.hash = "customer-view";
+  } else {
+    loginFailedMsg.innerText = serviceResponse.errorMsg ?? "jepjep"; //bo to jako default wartosc jakby nie bylo tej waidmosci, gdy null lub undefined
+    //paragraf sobie dorobic zeby wrzucac ten tekst
+    console.log("dupa");
+  }
+};
+
+//te url zmiany: uzywac tego HASH od location HASH (lokalizacja na stornie wiec nei strzela pod adres w necie tylko bedzie przechodzilo po tym dokumencie )
+// hash change event
+
+window.addEventListener(
+  "hashchange",
+  (event) => {
+    console.log(event);
+    const path = location.hash;
+
+    if (path.includes("cook") && userRole === "cook") {
+      console.log("poprawna rola");
+    } else if (path.includes("customer") && userRole === "customer") {
+      console.log("poprawna rola");
+    } else {
+      alert("niepoprawna rola");
+      const oldHash = event.oldURL.slice(event.oldURL.indexOf("#") + 1) ?? "";
+      location.hash = oldHash;
+      console.log(oldHash);
+    }
+  },
+  false
+);
+//tu musze jeszcze zrobic validacje czy gdy zmieni sie hash czy rola pozwala na wejscie na te podstorne czyli znowu zamokowac taka mape: rola + hashe, gdy sie nie zgodza to trzebaby cofnac na poprzedni adres
+//albo nie hash tylko objekt na window: history - i metody push state/replay state
+
+// const productsArray = [{name: 'kanapka1', composition: 'chleb,slonina,dzem', photo: },{name: 'kanapka2', composition: 'chleb,slonina,dzem', photo:}]
+
+// productsArray.forEach((product) =>{
+
+// })
